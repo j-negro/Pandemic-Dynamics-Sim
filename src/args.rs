8b71ab09367022bc715e48942d6f1ddf5342101d
Juan Negro <jnegro@itba.edu.ar>
2023-12-17 12:35:27 -0300

    Add rate validation to parser

--- src/args.rs
+++ src/args.rs
@@ -3,7 +3,6 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(name = "Pandemic Dynamics Sim", author, version, about)]
 pub struct Cli {
-
     // Output parameters
     #[arg(short, long, default_value_t = 1)]
     pub output_step_count: usize,
@@ -15,12 +14,21 @@ pub struct Cli {
     pub data_output_path: String,
 
     // Simulation input variables
-    #[arg(short, long, default_value_t = 0.5)]
+    #[arg(short, long, default_value_t = 0.5, value_parser = validate_rate)]
     pub transmission_rate: f64,
 
     #[arg(short, long, default_value_t = 7)]
     pub infectious_period: usize,
 
-    #[arg(short, long, default_value_t = 0.2)]
+    #[arg(short, long, default_value_t = 0.2, value_parser = validate_rate)]
     pub mortality_rate: f64,
+}
+
+
+fn validate_rate(rate: &str) -> Result<f64, String> {
+    let rate = rate.parse::<f64>().map_err(|_| "Rate specified is not a number.")?;
+    if rate < 0.0 || rate > 1.0 {
+        return Err("Please specify a number between 0 and 1.".to_owned())
+    }
+    Ok(rate)
 }
