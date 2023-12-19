3b9f6aa1df6b03e816fcb3e3276b2a88d0b1a314
Juan Negro <jnegro@itba.edu.ar>
2023-12-19 18:59:04 -0300

    Multiple runs per unique parameter config

--- analysis/run_simulation.py
+++ analysis/run_simulation.py
@@ -4,55 +4,67 @@ from concurrent.futures import ProcessPoolExecutor
 
 RESULTS_PATH = "./analysis/data/"
 
-DATA = [(1.2, 200), (1.8, 260), (2.4, 320), (3.0, 380)]
-
-TRANSMISSION_RATE_RANGE = [x / 10 for x in range(1, 9)]  # 0.4
-MORTALITY_RATE_RANGE = [x / 100 for x in range(1, 9)]  # 0.04
-INFECTION_PERIOD_RANGE = range(2, 7)  # 3
+TRANSMISSION_RATE_RANGE = [x / 100 for x in range(5, 55, 5)]  # 0.4
+MORTALITY_RATE_RANGE = [x / 100 for x in range(2, 22, 2)]  # 0.10
+INFECTION_PERIOD_RANGE = range(2, 8)  # 3
 
 TRANSMISSION_RATE_EXPERIMENT = [
     [
-        "-t",
-        str(x),
-        "-i",
-        "3",
-        "-m",
-        "0.05",
-        "--data-output-path",
-        RESULTS_PATH + "transmission/" + str(x) + ".txt",
+        [
+            "-t",
+            str(x),
+            "-i",
+            "3",
+            "-m",
+            "0.1",
+            "--data-output-path",
+            RESULTS_PATH + f"transmission/tra{x}_run{run}.txt",
+        ]
+        for x in TRANSMISSION_RATE_RANGE
+        for run in range(0, 3)
     ]
-    for x in TRANSMISSION_RATE_RANGE
 ]
 MORTALITY_RATE_EXPERIMENT = [
     [
-        "-t",
-        "0.4",
-        "-i",
-        "3",
-        "-m",
-        str(x),
-        "--data-output-path",
-        RESULTS_PATH + "mortality/" + str(x) + ".txt",
+        [
+            "-t",
+            "0.25",
+            "-i",
+            "3",
+            "-m",
+            str(x),
+            "--data-output-path",
+            RESULTS_PATH + f"mortality/mor{x}_run{run}.txt",
+        ]
+        for x in MORTALITY_RATE_RANGE
+        for run in range(0, 3)
     ]
-    for x in MORTALITY_RATE_RANGE
 ]
 INFECTION_PERIOD_EXPERIMENT = [
     [
-        "-t",
-        "0.4",
-        "-i",
-        str(x),
-        "-m",
-        "0.05",
-        "--data-output-path",
-        RESULTS_PATH + "period/" + str(x) + ".txt",
+        [
+            "-t",
+            "0.25",
+            "-i",
+            str(x),
+            "-m",
+            "0.1",
+            "--data-output-path",
+            RESULTS_PATH + f"period/per{x}_run{run}.txt",
+        ]
+        for x in INFECTION_PERIOD_RANGE
+        for run in range(0, 3)
     ]
-    for x in INFECTION_PERIOD_RANGE
 ]
 
 
 for name in ["transmission", "mortality", "period"]:
     os.makedirs(RESULTS_PATH + name, exist_ok=True)
+
+
+subprocess.run(
+    "cargo build -r",
+)
 
 
 def run_subprocess(arguments_list: list[str]):
